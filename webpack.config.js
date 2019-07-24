e0bc64fbe11b5847915a26cd6f581e805f30f323
Shreyans Jain <shreyansj551@gmail.com>
2019-07-24 13:21:49 +0530

    setup webpack for sass

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config;
